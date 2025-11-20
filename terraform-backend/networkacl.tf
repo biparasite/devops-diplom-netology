5566953fdfc07a9a685589e221f3065b6be1aea0
biparasite <117280737+biparasite@users.noreply.github.com>
2025-11-21 01:27:46 +0300

    Update networkacl.tf

--- terraform-backend/networkacl.tf
+++ terraform-backend/networkacl.tf
@@ -4,40 +4,34 @@ resource "yandex_vpc_security_group" "k8s-acl" {
   network_id  = yandex_vpc_network.develop.id
       ingress {
     protocol          = "ANY"
-    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
     predefined_target = "self_security_group"
     from_port         = 0
     to_port           = 65535
   }
     ingress {
     protocol       = "ANY"
-    description    = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
     v4_cidr_blocks = ["0.0.0.0/0"]
     from_port      = 30000
     to_port        = 32767
   }
   ingress {
     protocol       = "ANY"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
     v4_cidr_blocks = ["172.16.0.0/12", "10.0.0.0/8", "192.168.0.0/16"]
     port           = 6443
   }
 
   ingress {
     protocol       = "TCP"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
     v4_cidr_blocks = ["45.80.46.0/24"]
     port           = 443
   }
     ingress {
     protocol       = "TCP"
-    description    = "Правило разрешает подключение к узлам по SSH с указанных IP-адресов."
     v4_cidr_blocks = ["45.80.46.0/24"]
     port           = 22
   }
   egress {
     protocol       = "ANY"
-    description    = "Правило разрешает весь исходящий трафик"
     v4_cidr_blocks = ["0.0.0.0/0"]
     from_port      = 0
     to_port        = 65535
